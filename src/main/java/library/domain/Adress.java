dc22206c840bc8186323fad9025fafbb330a15da
wojtekj <s15983@pjwstk.edu.pl>
2017-10-04 13:50:26 +0200

    IS3 create profile and adress classes

--- src/main/java/library/domain/Adress.java
+++ src/main/java/library/domain/Adress.java
@@ -1,5 +1,40 @@
 package library.domain;
 
 public class Adress {
-
+	private String city;
+	private String postalCode;
+	private String street;
+	private String apNumber;
+	
+	public Adress(String city, String postalCode, String street, String apNumber) {
+		super();
+		this.city = city;
+		this.postalCode = postalCode;
+		this.street = street;
+		this.apNumber = apNumber;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getPostalCode() {
+		return postalCode;
+	}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getApNumber() {
+		return apNumber;
+	}
+	public void setApNumber(String apNumber) {
+		this.apNumber = apNumber;
+	}
 }
