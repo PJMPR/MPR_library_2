ef916eb04cea5c7f2ab48f44db8ea71c5a3b0a49
Beeert <beert13@o2.pl>
2017-10-05 19:32:01 +0200

    add Author and BookInformation

---
+++ src/main/java/library/domain/BookInformation.java
@@ -0,0 +1,26 @@
+package library.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookInformation {
+
+    private String title;
+    List<Author> authors = new ArrayList<Author>();
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<Author> authors) {
+        this.authors = authors;
+    }
+}
