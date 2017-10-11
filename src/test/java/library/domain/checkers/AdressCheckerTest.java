0d6ff53c848f7b9841876bf0ef692915106543ac
wojtekjPJATK <32327151+wojtekjPJATK@users.noreply.github.com>
2017-10-11 13:37:22 +0200

    Update AdressCheckerTest.java

--- src/test/java/library/domain/checkers/AdressCheckerTest.java
+++ src/test/java/library/domain/checkers/AdressCheckerTest.java
@@ -7,10 +7,11 @@ import library.domain.Author;
 import org.junit.Test;
 
 public class AdressCheckerTest {
-
+	AdressChecker checker = new AdressChecker();
+	
 	@Test
-	public void test() {
-		AdressChecker checker = new AdressChecker();
+	public void test_street() {
+		
 		
 		Adress adressWithNullStreet = new Adress();
 		Adress adressWithEmptyStringStreet = new Adress();
@@ -21,8 +22,9 @@ public class AdressCheckerTest {
 		
 		assertFalse("Street null field should be invalid", nullStreetIsValid);
 		assertFalse("Adress with empty street name should be invalid", isAStreetWithEmptyStringValid);
-		
-		// ==================================================
+	}
+	@Test
+	public void test_city() {
 		Adress adressWithNullCity = new Adress();
 		Adress adressWithEmptyStringCity = new Adress();
 		adressWithEmptyStringCity.setCity("");
@@ -32,8 +34,10 @@ public class AdressCheckerTest {
 		
 		assertFalse("City with null field should be invalid", nullCityIsValid);
 		assertFalse("Adress with empty city name should be invalid", isACityWithEmptyStringValid);
+	}
 		
-		// =====================================================
+	@Test
+	public void test_postal_code() {
 		Adress adressWithNullPostal = new Adress();
 		Adress adressWithEmptyStringPostal = new Adress();
 		adressWithEmptyStringPostal.setPostalCode("");
@@ -43,14 +47,16 @@ public class AdressCheckerTest {
 		
 		assertFalse("Postal code with null field should be invalid", nullPostalIsValid);
 		assertFalse("Adress with empty postal code name should be invalid", isAPostalWithEmptyStringValid);
-		
-		// ======================================================
+	}	
+	
+	@Test
+	public void test_apt_number() {
 		Adress adressWithNullApt = new Adress();
 		Adress adressWithEmptyStringApt = new Adress();
 		adressWithEmptyStringApt.setStreet("");
 		
 		boolean nullAptIsValid = checker.validAdressApt(adressWithNullApt);
-		boolean isAptWithEmptyStringValid = checker.validAdressStreet(adressWithEmptyStringStreet);
+		boolean isAptWithEmptyStringValid = checker.validAdressStreet(adressWithEmptyStringApt);
 		
 		assertFalse("Apt number with null field should be invalid", nullAptIsValid);
 		assertFalse("Adress with empty apt number should be invalid", isAptWithEmptyStringValid);
