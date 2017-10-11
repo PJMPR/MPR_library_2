de053e6bfa3076eff19e04fd8e788f25cfec7b13
Beeert <beert13@o2.pl>
2017-10-11 14:08:24 +0200

    improved BookCheckerTest

--- src/test/java/library/BookCheckerTest.java
+++ src/test/java/library/BookCheckerTest.java
@@ -3,10 +3,7 @@ package library.domain.checkers;
 import library.domain.*;
 import org.junit.jupiter.api.Test;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -82,122 +79,7 @@ class BookCheckerTest {
         BookChecker checker = new BookChecker();
 
         BookInformation BookChecker = new BookInformation();
-        BookChecker.setAuthors(new List<Author>() {
-            @Override
-            public int size() {
-                return 0;
-            }
-
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-
-            @Override
-            public boolean contains(Object o) {
-                return false;
-            }
-
-            @Override
-            public Iterator<Author> iterator() {
-                return null;
-            }
-
-            @Override
-            public Object[] toArray() {
-                return new Object[0];
-            }
-
-            @Override
-            public <T> T[] toArray(T[] a) {
-                return null;
-            }
-
-            @Override
-            public boolean add(Author author) {
-                return false;
-            }
-
-            @Override
-            public boolean remove(Object o) {
-                return false;
-            }
-
-            @Override
-            public boolean containsAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(Collection<? extends Author> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(int index, Collection<? extends Author> c) {
-                return false;
-            }
-
-            @Override
-            public boolean removeAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean retainAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public void clear() {
-
-            }
-
-            @Override
-            public Author get(int index) {
-                return null;
-            }
-
-            @Override
-            public Author set(int index, Author element) {
-                return null;
-            }
-
-            @Override
-            public void add(int index, Author element) {
-
-            }
-
-            @Override
-            public Author remove(int index) {
-                return null;
-            }
-
-            @Override
-            public int indexOf(Object o) {
-                return 0;
-            }
-
-            @Override
-            public int lastIndexOf(Object o) {
-                return 0;
-            }
-
-            @Override
-            public ListIterator<Author> listIterator() {
-                return null;
-            }
-
-            @Override
-            public ListIterator<Author> listIterator(int index) {
-                return null;
-            }
-
-            @Override
-            public List<Author> subList(int fromIndex, int toIndex) {
-                return null;
-            }
-        });
+        BookChecker.setAuthors(new ArrayList<Author>());
 
         boolean isListValid = checker.validList(BookChecker);
 
