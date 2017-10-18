235992d4ad556f1d4fc78f606ff8013965f782b7
s15664 <s15664@pjwstk.edu.pl>
2017-10-18 12:40:12 +0200

    IS 40 add Notification, BorrowingOrder and ReservationOrder tables

--- src/main/java/library/App.java
+++ src/main/java/library/App.java
@@ -1,6 +1,9 @@
 package library;
 
 import library.dao.repositories.AuthorRepository;
+import library.dao.repositories.BorrowingOrderRepository;
+import library.dao.repositories.NotificationRepository;
+import library.dao.repositories.ReservationOrderRepository;
 
 /**
  * Hello world!
@@ -12,6 +15,15 @@ public class App
     {
     	AuthorRepository authorRepository = new AuthorRepository();
     	authorRepository.createTable();
+    	
+    	NotificationRepository notificationRepository = new NotificationRepository();
+    	notificationRepository.createTable();
+    	
+    	BorrowingOrderRepository borrowingOrderRepository = new BorrowingOrderRepository();
+    	borrowingOrderRepository.createTable();
+    	
+    	ReservationOrderRepository reservationOrderRepository = new ReservationOrderRepository();
+    	reservationOrderRepository.createTable();
     	
         System.out.println( "Koniec" );
     }
