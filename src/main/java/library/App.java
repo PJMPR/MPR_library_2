c8bc3796e3c94f1e509bc7fc1a92263f9a0e0258
aurbanow <aurbanow.pjwstk.edu.pl>
2017-10-18 12:09:41 +0200

    IS37 add author table

--- src/main/java/library/App.java
+++ src/main/java/library/App.java
@@ -1,5 +1,7 @@
 package library;
 
+import library.dao.repositories.AuthorRepository;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	AuthorRepository authorRepository = new AuthorRepository();
+    	authorRepository.createTable();
+    	
+        System.out.println( "Koniec" );
     }
 }
