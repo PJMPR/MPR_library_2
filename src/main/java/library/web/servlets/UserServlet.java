958fdca9b7966b56cfad2e358ca5a932c3425e04
visidor97 <s15463@pjwstk.edu.pl>
2017-12-06 14:12:47 +0100

    IS198 UserServlet fixed

--- src/main/java/library/web/servlets/UserServlet.java
+++ src/main/java/library/web/servlets/UserServlet.java
@@ -9,6 +9,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import library.dao.repositories.IDatabaseCatalog;
+import library.dao.repositories.impl.HsqlCatalogFactory;
+import library.domain.User;
+
 
 @WebServlet("UserServlet")
 public class UserServlet extends HttpServlet {
@@ -24,12 +28,23 @@ public class UserServlet extends HttpServlet {
 		
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
+		IDatabaseCatalog library = new HsqlCatalogFactory().library();
 		String nickname = request.getParameter("nickname");
 		String password = request.getParameter("password");
+		User user = new User();
 		
 		if(nickname!=null && !nickname.isEmpty() 
 				&& password!=null && !password.isEmpty()){
 			out.println("Login: "+ nickname + "<br>Password: "+ password);
+			user.setLogin(nickname);
+			user.setPassword(password);
+			library.users().add(user);
+			library.saveChanges();
+			out.println("<br>Wprowadzono dane poprawnie.");
+			
+		}
+		else{
+			out.println("<br>Wprowadzono dane niepoprawnie.");
 		}
 		
 	}
