48819256032e67c80b4f09e0cefbb42d5a31b5be
Michal <s15667@pjwstk.edu.pl>
2018-01-17 13:24:47 +0100

    author

--- src/main/java/library/domain/Author.java
+++ src/main/java/library/domain/Author.java
@@ -1,11 +1,32 @@
 package library.domain;
 
-public class Author implements IHaveId {
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="authors")
+@NamedQueries({
+	@NamedQuery(name="authors.all", query="SELECT a FROM Author a"),
+	@NamedQuery(name="authors.id", query="SELECT a FROM Author a WHERE a.id=:id")
+})
+public class Author implements IHaveId {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
     private String name;
     private String secondName;
     private String surname;
+    
+    @ManyToMany(mappedBy="books")
+    private List<Book> books;
 
     public Author(String name, String secondName, String surname) {
         this.name = name;
@@ -14,9 +35,17 @@ public class Author implements IHaveId {
     }
 
     public Author() {
+    	super();
+	}
+   
+	public List<Book> getBooks() {
+		return books;
 	}
 
-    
+	public void setBooks(List<Book> books) {
+		this.books = books;
+	}
+
 	public int getId() {
 		return id;
 	}
---
+++ src/main/java/library/web/rest/AuthorsResources.java
@@ -0,0 +1,95 @@
+package library.web.rest;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import library.domain.Author;
+@Path("/authors")
+@Stateless
+public class AuthorsResources {
+	@PersistenceContext
+	EntityManager mgr;
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getAll(@QueryParam("page") int page,@QueryParam("max") int max){
+		
+		List<Author> authors = mgr
+				.createNamedQuery("authors.all", Author.class)
+				.getResultList();
+		if(authors.isEmpty())
+			return Response.status(404).build();
+		return Response.ok(new GenericEntity<List<Author>>(authors){}).build();
+	}	
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{id}")
+	public Response get(@PathParam("id") int id){
+		List<Author> authors = mgr.createNamedQuery("authors.id",Author.class)
+				.setParameter("id", id)
+				.getResultList();
+		if(authors.size()==0)
+			return Response.status(404).build();
+		return Response.ok(authors.get(0)).build();
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response addUser(Author author){
+		mgr.persist(author);
+		return Response.ok().build();
+	}
+	
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/{id}")
+	public Response updateAuthor(@PathParam("id") int id ,Author author){
+		List<Author> authors = mgr.createNamedQuery("authors.id",Author.class)
+				.setParameter("id", id)
+				.getResultList();
+		if(authors.size()==0)
+			return Response.status(404).build();
+		Author a = authors.get(0);
+		a.setName(author.getName());
+		a.setSurname(author.getSurname());
+		a.setSecondName(author.getSecondName());
+		
+		mgr.persist(a);
+		return Response.ok().build();
+	}
+	
+	@DELETE
+	@Path("/{id}")
+	public Response deleteUser(@PathParam("id") int id){
+		List<Author> authors = mgr.createNamedQuery("users.id",Author.class)
+				.setParameter("id", id)
+				.getResultList();
+		if(authors.size()==0)
+			return Response.status(404).build();
+		mgr.remove(authors.get(0));
+		return Response.noContent().build();
+	}
+	
+	@GET
+	@Path("/status")
+	@Produces(MediaType.TEXT_HTML)
+	public String test(){
+		return "OK";
+	} 
+}
